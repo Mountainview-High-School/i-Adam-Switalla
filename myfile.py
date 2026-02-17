4106853d8fc067a224ef6a7ac8dfd570ac9f596e
MVHS_Litlight <159203470+Adam-Switalla@users.noreply.github.com>
2026-02-17 13:23:18 +1300

    Added albums and some tracks

---
+++ myfile.py
@@ -0,0 +1,7 @@
+print('Welcome to the world of the plastic beach!')
+albums = ['Phase 1 2001 - Self Titled', 'Phase 2 2005 - Demon Days', 'Phase 3 2011 - Plastic Beach, The Fall', 'Phase 4 2016 - Humanz', 'Phase 5 2019- The Now Now', 'Phase 6 2020 - The Song Machine Season 1 - Strange Timez', 'Phase 7 2023 - Cracker Island', 'Phase 8 2026 - The Mountain']
+for album in albums:
+#albums.reverse()
+    print(album)
+
+Tracklist_Self_Titled = ['Re-Hash - 3:40', '5/4 - 2:42', 'Tommorow Comes Today - 3:13', 'New Genious (Brother) - 3:59', 'Clint Eastwood - 5:41', 'Man Research (Clapper) - 4:30', 'Punk - 1:36', 'Sound Check (Gravity) - 4:41',  ]
