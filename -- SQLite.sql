98e8f260a25d9f4afe3f78870a5c511904c1fbc5
MVHS_Litlight <159203470+Adam-Switalla@users.noreply.github.com>
2026-03-10 12:40:44 +1300

    Answered a few question

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -1,9 +1,20 @@
 -- SQLite
--- 1
+--1
 
-select * from student order by date_of_birth
+select * from student order by date_of_birth;
 
--- 2
+--2
 
-select * from student WHERE first_name = gene;
+select * from student WHERE first_name = 'Gene';
 
+--3
+
+select * from student WHERE  last_name LIKE "%y";
+
+--4
+
+select * from class WHERE NOT class_name = "44";
+
+--5
+
+select * from class WHERE class_id = "44" OR class_id = "43";
